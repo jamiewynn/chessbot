70610fafe7ed53551f96a8e4756998cb8c62507c
Jamie Wynn <jamie.wynn@five.ai>
2021-04-02 13:21:34 +0100

    Get CI working

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import setuptools
+
+repository_dir = os.path.dirname(__file__)
+with open(os.path.join(repository_dir, "requirements.txt")) as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="chessbot",
+    version='0.0.0',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    setup_requires=['wheel'],
+    install_requires=requirements,
+)
